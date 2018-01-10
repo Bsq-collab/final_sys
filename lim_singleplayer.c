dd6d5c15d4f57caf5ccc51c3289f5e11770bb5f6
BrianLim167 <blim167@gmail.com>
2018-01-10 15:35:44 -0500

    finished ask, began slow_print

--- lim_singleplayer.c
+++ lim_singleplayer.c
@@ -1,7 +1,21 @@
-#include final.h
+#include "final.h"
 
-char buzz() {
-  printf("SAMPLE QUESTION\n");
+void slow_print(char * str) {
+  int last = 0;
+  while ( str[last] ) {
+    char part[256];
+    memset(part, 0, 256);
+    strncpy(part, str, last+1);
+    usleep(100000);
+    //execvp("clear", "clear");
+    printf("%s\n", part);
+    last++;
+  }
+}
+
+char ask(char * q_and_a) {
+  //printf("%s\n", q_and_a);
+  slow_print(q_and_a);
   char buf[256];
   fgets(buf, 256, stdin);
   printf("Your answer: ");
@@ -10,7 +24,7 @@ char buzz() {
 }
 
 int main() {
-  printf("%s\n", buzz());
+  printf("@%c\n", ask("THIS IS A SAMPLE QUESTION"));
   
   return 1;
 }
