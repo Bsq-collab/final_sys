5e90f6a64eb72f29cd4a0c4fb807dc970fd309bc
BrianLim167 <blim167@gmail.com>
2018-01-17 15:35:55 -0500

    attempted fix for slow_print

--- lim_singleplayer.c
+++ lim_singleplayer.c
@@ -7,15 +7,16 @@ void slow_print(char * str) {
     memset(part, 0, 256);
     strncpy(part, str, last+1);
     usleep(100000);
-    //execvp("clear", "clear");
+    char arg[1][5] = {"clear"};
+    //execlp(arg, "clear");
     printf("%s\n", part);
     last++;
   }
 }
 
 char ask(char * q_and_a) {
-  //printf("%s\n", q_and_a);
-  slow_print(q_and_a);
+  printf("%s\n", q_and_a);
+  //slow_print(q_and_a);
   char buf[256];
   fgets(buf, 256, stdin);
   printf("Your answer: ");
